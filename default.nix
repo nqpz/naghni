844f9cbcad2c753798dd197140d8a93632d91f22
Niels G. W. Serup <ngws@metanohi.name>
2020-08-25 22:08:22 +0200

    Simplify Nix.

--- default.nix
+++ default.nix
@@ -1,11 +1,5 @@
 with import <nixpkgs> {};
 stdenv.mkDerivation {
     name = "naghni";
-    buildInputs = [ (python27.withPackages (ps: with ps; [ pygame pillow pycairo numpy pygobject3 termcolor setproctitle ]))
-                    gnome2.python_rsvg
-                  ];
-
-    shellHook = ''
-      export PYTHONPATH=${gnome2.python_rsvg}/lib/python2.7/site-packages/gtk-2.0:$PYTHONPATH
-    '';
+    buildInputs = [ (python27.withPackages (ps: with ps; [ pygame pillow pycairo numpy pygobject3 termcolor setproctitle ])) ];
 }
